033f0b137385ac638b61361da75f1221f18a2dce
Aryanjan002 <74414155+Aryanjan002@users.noreply.github.com>
2020-11-13 18:04:57 +0430

    Update Aryan.sh

--- Aryan.sh
+++ Aryan.sh
@@ -2,19 +2,19 @@
 
 
 #Mod by        : Aryan
-#Tool Unfaedah : h4ker;"A"
+#Tool Unfaedah : Aryan Hack;"A"
 
 clear
 
                                           
 
- echo $mer"               WhatsappHack by Aryan | t.me/h4kerAryan          "
+ echo $mer"               WhatsappHack by Aryan | t.me/Aryan Hack          "
 
 
  echo ""
  echo -n $cy" Enter Whatsapp Number for hack: "
  read t
- echo $pur"<•=============h4ket============•>"
+ echo $pur"<•=============AryanHacker============•>"
  echo -n $cy" Click on verification on ok Whatsapp and then tipe ok: "
  read t
  echo $pur"<•===========AryanHacker============•>"
