55e9e9a1c031c9fbe232ef653b5ab3f76f470ce3
Aryanjan002 <74414155+Aryanjan002@users.noreply.github.com>
2020-11-13 18:00:51 +0430

    Create Aryan.sh

---
+++ Aryan.sh
@@ -0,0 +1,26 @@
+
+
+
+#Mod by        : Aryan
+#Tool Unfaedah : h4ker;"A"
+
+clear
+
+                                          
+
+ echo $mer"               WhatsappHack by Aryan | t.me/h4kerAryan          "
+
+
+ echo ""
+ echo -n $cy" Enter Whatsapp Number for hack: "
+ read t
+ echo $pur"<•=============h4ket============•>"
+ echo -n $cy" Click on verification on ok Whatsapp and then tipe ok: "
+ read t
+ echo $pur"<•===========AryanHacker============•>"
+ echo $me "Verification Code loading "
+ sleep 10
+ 
+ #!/bin/bash
+
+ echo Code:$((RANDOM + 456907))
